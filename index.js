6ec2843a3e9661527edf6c92c2f356b214029597
Gray.1991 <Sergey.sp.1991@gmail.com>
2022-04-07 22:24:31 +0300

    shema date, model

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
+import Post from "./Post.js";
 
-const PORT = 5000;
+const PORT = 6000;
 const DB_URL = `mongodb+srv://Gray1991:z1x2c3v4@node.hs7xi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.status(200).json("Сервер работает");
+app.post("/", async (req, res) => {
+  try {
+    const { author, title, content, picture } = req.body;
+    const post = await Post.create({ author, title, content, picture });
+    res.json(post);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
 
 async function startApp() {
