1c262b783ad63489e93ec6c6c3ac33ad95badad8
Gray.1991 <Sergey.sp.1991@gmail.com>
2022-04-07 20:00:18 +0300

    mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const PORT = 5000;
+const DB_URL = `mongodb+srv://Gray1991:z1x2c3v4@node.hs7xi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const app = express();
 
+app.use(express.json());
+
 app.post("/", (req, res) => {
   console.log(req.body);
-  res.status(200).json("server working123");
+  res.status(200).json("Сервер работает");
 });
 
-app.listen(PORT, () => console.log("server started on " + PORT));
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log("server started on " + PORT));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+startApp();
